e4236b7442f8e3145844cccabff62b9ba41a7855
Sean Stephens <seanstephens29@gmail.com>
2015-04-22 14:16:09 -0600

    fixed things

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
   {
 	this._super.included(app);
 
-	app.import(app.bowerDirectory + '/torpid-js/torpid-js.min.js');
+	app.import(app.bowerDirectory + '/torpid-js/torpid-js.js');
 	app.import(app.bowerDirectory + '/torpid-js/torpid-js.css');
   }
 };

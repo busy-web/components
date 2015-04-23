edf04ed6f1c23480a0f29c17ee4c28680ca94625
Sean Stephens <seanstephens29@gmail.com>
2015-04-23 16:18:01 -0600

    fixes

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,9 +6,6 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
-	sassOptions: {
-		outputFile: 'dummy.css'
-	},
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build

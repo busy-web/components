3e057fa51ff99d6acee1fc5e9912c993b0ba192e
Jacob Pedersen <you@example.com>
2015-05-22 11:02:27 -0600

    check

--- addon/components/torpid-checkbox.js
+++ addon/components/torpid-checkbox.js
@@ -18,6 +18,20 @@ export default Ember.Component.extend({
 
   disabled: false,
 
-  checked: false
+  checked: false,
+
+  sendChange: function()
+  {
+    this.set('checked', !this.get('checked'));
+  },
+
+  actions: {
+    sendChecked: function()
+    {
+      this.sendChange();
+      console.log(this.get('checked'));
+      this.sendAction();
+    }
+  }
 
 });

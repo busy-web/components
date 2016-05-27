5443b001a8b0e8fd53eb3eb0a4bc79ad2bd86cb0
Sean Stephens <seanstephens29@gmail.com>
2016-05-27 16:33:33 -0600

    fixes to app

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -3,17 +3,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend(
 {
-
 	model: function()
 	{
-		return new Ember.RSVP.Promise(function(resolve)
+		return Ember.RSVP.hash(
 		{
-			Ember.run(null, resolve, Ember.A([
+			people: Ember.A([
 				Ember.Object.create({name: 'Bob Thomas'}),
 				Ember.Object.create({name: 'John Smith'}),
 				Ember.Object.create({name: 'Bill Billstien'}),
 				Ember.Object.create({name: 'Jerry Gary'}),
-			]));
+			]),
+
+			select: Ember.A([
+				Ember.Object.create({label: "test 1", key: 0}),
+				Ember.Object.create({label: "test 2", key: 0}),
+				Ember.Object.create({label: "test 3", key: 0}),
+			])
 		});
 	}
 });

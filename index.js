c0d1f7f10120bcf6396bb6a9a09f5d08d550b7b9
Sean Stephens <seanstephens29@gmail.com>
2016-12-20 18:33:06 -0700

    chore(updates): updated ember-cli 2.10.0

--- index.js
+++ index.js
@@ -2,10 +2,5 @@
 'use strict';
 
 module.exports = {
-	name: 'busy-components',
-
-	included: function(app /*, parentAddon*/)
-	{
-		return this._super.included(app);
-	}
+  name: 'busy-components'
 };

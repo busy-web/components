b0d27168cc480c513270a34f4373817b3f989da9
Keith Allen <robotsguide@gmail.com>
2017-07-25 13:54:28 -0600

    feat (component): add min option to bc-input for numbers

--- addon/components/bc-input.js
+++ addon/components/bc-input.js
@@ -16,7 +16,7 @@ export default Ember.TextField.extend({
 	tagName: 'input',
 	classNames: ['bc-input'],
 
-	attributeBindings: ['autofocus', 'maxlength', 'placeholder', 'type', 'tabindex', 'autocomplete', 'disabled'],
+	attributeBindings: ['autofocus', 'maxlength', 'placeholder', 'type', 'tabindex', 'autocomplete', 'disabled', 'min'],
 
 	autofocus: '',
 	type: 'text',
@@ -26,6 +26,7 @@ export default Ember.TextField.extend({
 	autocomplete: '',
 	maxlength: '',
 	disabled: false,
+	min: '',
 
 	focusOut() {
 		this.sendAction('onBlur', this.getVal());

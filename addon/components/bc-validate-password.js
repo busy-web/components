4f7c4cf4892d9bd55921a2880c067569e991b88c
Sean Stephens <seanstephens29@gmail.com>
2018-02-19 11:52:39 -0700

    fix(component): text grammer incorrect in password input

--- addon/components/bc-validate-password.js
+++ addon/components/bc-validate-password.js
@@ -8,13 +8,12 @@ import BCValidate from './bc-validate';
  * `BusyComponents/Component/BCValidatePassword`
  *
  */
-export default BCValidate.extend(
-{
+export default BCValidate.extend({
 	validateExpression: /^(?=.*[0-9])(?=.*[a-zA-Z])(\w|[!@#$%^&*?_~-]).{5,}$/,
 
 	type: 'password',
 
-	invalidError: 'Password must contain at a number, a letter, and be more than 6 characters long.',
+	invalidError: 'Password must contain a number, a letter, and be more than 6 characters long.',
 
 	placeholder: 'Password',
 });
